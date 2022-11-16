2a1246d66b6aeb85b02bb9a536146fbbe9e475dc
Jameslinimk <jameskimimk@gmail.com>
2022-11-15 23:16:40 -0500

    S

--- check.ts
+++ check.ts
@@ -1,15 +1,14 @@
 import chalk from "chalk"
 import detectPort from "detect-port"
-import { existsSync } from "fs"
 import { projects } from "./index.js"
 
-if (existsSync("/usr/local/var/run/nginx.pid")) {
-    console.log(chalk.red("nginx is running"))
-} else {
-    console.log(chalk.red("nginx is not running"))
-}
-
-for (const project of projects) {
+for (const project of [
+    ...projects,
+    {
+        name: "nginx",
+        port: 80,
+    },
+]) {
     const port = await detectPort(project.port)
     if (port !== project.port) {
         console.log(chalk.green(`${project.name} is running`))
